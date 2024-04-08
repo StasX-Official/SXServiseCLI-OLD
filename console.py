2be1f56b420fbbd491c5f87a40f6ba8de32ea2e3
StasX <141828909+StasX-Official@users.noreply.github.com>
2024-04-08 18:43:48 +0300

    THE END
    ATTENTION! Support for the old version of SXServiseCLI has ended on April 8, 2024. We sincerely thank you for using our application during this time.
    
    If you are still using the old version, we recommend updating your program to the new version of SXServiseCLI 2024. The new version offers more features, improved performance, and ensures security.
    
    To update to the new version, visit the application page on GitHub -> https://github.com/StasX-Official/SXServiseCLI. If you have any questions or issues with the update, please contact our support at sxservise@outlook.com.
    
    Once again, thank you for using SXServiseCLI. We are always ready to assist you in your user experience!
    
    Sincerely,
    StasX

--- console.py
+++ console.py
@@ -30,8 +30,16 @@ app_ver="4.14"
 
 #Project developer: StasX
 #The project is under development
-#App version - v4.15Beta 
+#App version - v2.15Beta 
 #Copying the code is prohibited by SX copyright.
+
+#ATTENTION! Support for the old version of SXServiseCLI has ended on April 8, 2024. We sincerely thank you for using our application during this time.
+#If you are still using the old version, we recommend updating your program to the new version of SXServiseCLI 2024. The new version offers more features, improved performance, and ensures security.
+#To update to the new version, visit the application page on GitHub -> https://github.com/StasX-Official/SXServiseCLI. If you have any questions or issues with the update, please contact our support at sxservise@outlook.com.
+#Once again, thank you for using SXServiseCLI. We are always ready to assist you in your user experience!
+#Sincerely,
+#StasX
+
 
 init(autoreset=True)
 ModuleNotFoundError = "Plugin not found. Install the plugin on our website https://sxcomp.42web.io/ or contact SX technical support."
